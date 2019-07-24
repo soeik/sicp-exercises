58321b1b80c1b07ddd54252c30c77c927305f959
Stepanov <kirill.stepanov@t-systems.ru>
2019-07-24 17:28:58 +0300

    Algorithm improved

--- square-root.rkt
+++ square-root.rkt
@@ -1,16 +1,16 @@
 #lang sicp
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0 x x))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt-iter guess previous-guess x)
+  (if (good-enough? guess previous-guess x)
       guess
-      (sqrt-iter (improve guess x)
+      (sqrt-iter (improve guess x) guess
                  x)))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess previous-guess x)
+  (< (abs (- guess previous-guess)) 0.000000001))
 
 (define (square x)
   (* x x))
